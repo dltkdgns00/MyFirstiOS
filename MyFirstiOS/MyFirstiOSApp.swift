385193a8fdc37d23aa5c15473234893d01a6d8a6
dltkdgns00 <s20604@naver.com>
2023-05-03 19:15:56 +0900

    Initial Commit

---
+++ MyFirstiOS/MyFirstiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstiOSApp.swift
+//  MyFirstiOS
+//
+//  Created by 이상훈 on 2023/05/03.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
